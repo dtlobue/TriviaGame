66cf8ad12a535774336fd5e268f3b5b0a80f5545
Daniel Lo Bue <dtlobue@gmail.com>
2017-07-09 21:37:38 -0700

    worked on design and some logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,39 @@
-//Global Scope: Variables to be declared
-var counter = 100;
+// Advanced Trivia Pseudocode:
+// Any Global Variables (arrays/objects of questions?)
+var counter = 30;
+var firstQ = {
+	question: "Which New Wave artist from the UK wrote the hit single 'Cars'?",
+	one: "Colin Newman",
+	two: "Gary Numan",
+	three: "Adam Ant",
+  four: "Morrissey"
+};
 
-//Events: Start Game, End Game
-	//Start Game: can only happen 1 way, when Start Button is clicked
-	document.getElementById("startButton").addEventListener("click", startGame);
-	//this is clearly on 'on.click' event, with event listener (above)
-	
-	//A number of things happen when Start Game is initiated:
-	function startGame() {
-		//The timer starts running from its set time down toward 0
-			//Were it to hit 0, the 'End Game' event would ensue, 
-			//no matter if all questions have been filled.
-			//Seems like a decrementing setTimeout.
+function startGame() {
+	$(#questionBox).text(firstQ);
+};
 
-		//The trivia questions and answers are displayed
-			//The answers are radio buttons
-			//only one of them may be chosen
-		//The Done Button is displayed at the bottom
-			//If clicked, 'End Game' event ensues
 
-	}
-		//The timer starts running from its set time down toward 0
-			//Were it to hit 0, the 'End Game' event would ensue, 
-			//no matter if all questions have been filled.
-			//Seems like a decrementing setTimeout.
-		//The trivia questions and answers are displayed
-			//The answers are radio buttons
-			//only one of them may be chosen
-		//The Done Button is displayed at the bottom
-			//If clicked, 'End Game' event ensues
-	//End Game: can happen 2 ways, as stated above
-		//Displays: Correct Answers, Incorrect Answers, Unanswered
-			//Must connect question choices to this list
+
+// Initial Page has Start Button that initiates first round/question
+$('#startButton').on('click', function(event) {
+	startGame();
+});
+// Should startGame be a function? Usable for "Start Over?", below
+
+
+// One question shown at a time. Each question has a timer counting down, four
+// potential answers (that are clickable)
+
+// Each question leads to a declaration on the page; wrong answers will say
+// "Nope!" and display the correct answer, possibly with image. Correct answers
+// will declare "Correct!". A brief interval will ensue (about 5 secs). After
+// both, the page will transition to the next question WITHOUT user input
+// (i.e, using set Timeout). Also, if the timer runs
+// out, it will declare "Out of Time!" and also display correct answer.
+
+// End of Game: timer stops counting down and page displays Correct, Incorrect,
+// and Unanswered questions. Thus, there must be some kind of recording going on
+// while user is playing. The end page also has a "Start Over?" button that will
+// start the game over again WITHOUT reloading the page, much like the Start
+// Button at the beginning.
